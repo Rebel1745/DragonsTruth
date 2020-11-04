24175899372a9bb570aae7b6d968ded5defa9c13
Rebel1745 <31040719+Rebel1745@users.noreply.github.com>
2020-11-04 16:15:03 +0000

    Basic movement and animations
    Booyah!

---
+++ DragonsTruth/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Awake()
+    {
+        rb = GetComponentInChildren<Rigidbody2D>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        if (sr == null)
+        {
+            Debug.LogError("No sprite renderer found");
+        }
+    }
+
+    private void Start()
+    {
+        currentSpeed = Speed;
+    }
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
+    public float Speed = 3f;
+    public float SprintSpeed = 4f;
+    float currentSpeed;
+
+    public float JumpForce = 10f;
+    public int ExtraJumps;
+    int extraJumpValue;
+    public float FallMultiplier = 2.5f;
+    public float LowJumpMultiplier = 2f;
+
+    float moveInput;
+    bool isFacingRight = true;
+
+    public bool isGrounded;
+    public Transform GroundCheck;
+    public float CheckRadius;
+    public LayerMask WhatIsGround;
+
+    // Animation
+    public Animator anim;
+
+    private void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * currentSpeed, rb.velocity.y);
+        anim.SetFloat("Speed", Mathf.Abs(moveInput));
+    }
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(GroundCheck.position, CheckRadius, WhatIsGround);
+
+        // Jump
+        // if we are on the ground, we can jump
+        if (isGrounded)
+        {
+            extraJumpValue = ExtraJumps;
+            anim.SetBool("Jump", false);
+        }
+        else
+            anim.SetBool("Jump", true);
+
+        if (isGrounded && Input.GetButton("Jump"))
+        {
+            rb.velocity = Vector2.up * JumpForce;
+        }
+        if (Input.GetButtonDown("Jump") && extraJumpValue > 0)
+        {
+            rb.velocity = Vector2.up * JumpForce;
+            extraJumpValue--;
+        }
+        else if (Input.GetButtonDown("Jump") && extraJumpValue == 0 && isGrounded)
+        {
+            rb.velocity = Vector2.up * JumpForce;
+        }
+
+        // if jumping or falling, alter gravity to allow for medium and large jumps
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * FallMultiplier * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * LowJumpMultiplier * Time.deltaTime;
+        }
+        // END Jump
+
+        // If the input is moving the player right and the player is facing left...
+        if (moveInput > 0 && !isFacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        // Otherwise if the input is moving the player left and the player is facing right...
+        else if (moveInput < 0 && isFacingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        isFacingRight = !isFacingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+
+        //sr.flipX = !sr.flipX;
+    }
+}
